c6a27dc13560b5771a930dff10731d4dd3c80a78
ahinsutime <ahinsutime@gmail.com>
2023-11-01 15:11:40 +0900

    Day12: Basic user views (listview/gridview/pageview), dialog and tapbar

---
+++ dart_practice/day12_listview_gridview_pageview_dialog_tapbar/ch12_5_tab/test.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<MyApp> with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("TEST")
+          ),
+          bottom: TabBar(
+            controller: controller,
+            tabs: const <Widget>[
+              Tab(text: "One"),
+              Tab(text: "Two"),
+              Tab(text: "Three"),
+            ],
+          )
+        ),
+        body: TabBarView(
+          controller: controller,
+          children: const <Widget>[
+            Center(
+              child: Text(
+                "One Screen",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              )
+            ),
+            Center(
+              child: Text(
+                "Two Screen",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              )
+            ),
+            Center(
+              child: Text(
+                "Three Screen",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              )
+            )
+          ],
+        )
+      )
+    );
+  }
+}
