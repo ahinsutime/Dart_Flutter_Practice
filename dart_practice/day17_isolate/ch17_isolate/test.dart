c0075a0d6ea9fd9f5bda6ad65ef512a280b8237e
ahinsutime <ahinsutime@gmail.com>
2023-11-03 17:59:28 +0900

    Day17: Basic isolate usages(receivePort/sendPort/first/listen/spawn)

---
+++ dart_practice/day17_isolate/ch17_isolate/test.dart
@@ -0,0 +1,85 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'dart:isolate';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+myIsolate(SendPort mainPort) {
+  ReceivePort isoPort = ReceivePort();
+  mainPort.send({'port': isoPort.sendPort});
+  isoPort.listen((message) {
+    if (message['msg'] != "bye") {
+      int count = message['msg'];
+      mainPort.send({'msg': count * count});
+    } else {
+      isoPort.close();
+    }
+  });
+}
+
+class MyAppState extends State<MyApp> {
+  String result = "";
+
+  void onPress() async {
+    ReceivePort mainPort = ReceivePort();
+    await Isolate.spawn(myIsolate, mainPort.sendPort);
+
+    SendPort? isoPort;
+    mainPort.listen((message){
+      if (message['port'] != null) {
+        isoPort= message['port'];
+      } else if (message['msg'] != null) {
+        setState(() {
+          result = "msg: ${message['msg']}";
+        });
+      }
+    });
+
+    int count = 0;
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      count++;
+      if (count < 6) {
+        isoPort?.send({'msg': count});
+      } else {
+        isoPort?.send({'msg': 'bye'});
+        mainPort.close();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("TEST"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                result,
+                style: const TextStyle(fontSize: 30),
+              ),
+              ElevatedButton(
+                  onPressed: onPress,
+                  child: const Text("TEST1"),
+              )
+            ],
+          )
+        )
+      )
+    );
+  }
+
+}
